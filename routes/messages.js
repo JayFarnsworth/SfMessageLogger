11388296980adf928fad1c1e2ea85d7017f6bdeb
JayFarnsworth <JCFarnz@gmail.com>
2018-11-20 12:10:15 -0700

    testing

--- routes/messages.js
+++ routes/messages.js
@@ -18,6 +18,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
+    console.log(req)
     collection.insert({
         IntegrationId: req.body.IntegrationId,
         EntityType: req.body.EntityType,
